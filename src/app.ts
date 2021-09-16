78b71262ea39ffcd7272d08b9a42cea3f5086d49
matxa <42968545+matxa@users.noreply.github.com>
2021-09-15 21:17:49 -0400

    add cors to express

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from 'path';
+import cors from 'cors';
 import DB from "./dao/db";
 import userRoutes from "./controller/userController";
 import debtCardRoutes from "./controller/debtController";
@@ -14,6 +15,7 @@ const host: string = process.env.HOST || "localhost";
 
 // Application Configurations
 const app: express.Application = express();
+app.use(cors());
 app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/debtcards", debtCardRoutes);
